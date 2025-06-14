9c5ccec32d086c08c50d0246aff3cec7a3d9f1c0
Eduard <ed.lyubarets@gmail.com>
2025-06-14 18:55:53 +0300

    lab_2_2

---
+++ lab_2_2/lab_2_calc_integral.c
@@ -0,0 +1,98 @@
+﻿#include<stdlib.h>  //директива препроцесора
+#include<stdio.h>
+#include<math.h>
+#include"lab_2_calc_integral.h"
+
+double myFunction(double x) {		
+	return pow(x, 2) + 2 * x;
+}
+
+double method_A(double firstArgument, double lastArgument, int precision) {
+	double segmentValue = (lastArgument - firstArgument) / precision; //довжина сегменту
+	double functionResult;          //висота сегменту
+	double sum = 0;               //сума площ сегментів(результат розрахунку)
+	double x = firstArgument;
+
+	while (x <= lastArgument - segmentValue) {
+		functionResult = myFunction(x);
+		sum += functionResult;
+		x += segmentValue;
+	}
+
+	return segmentValue * sum; //площа сегментів
+}
+
+double method_B(double firstArgument, double lastArgument, int precision) {
+	double segmentValue = (lastArgument - firstArgument) / precision; //довжина сегменту
+	double functionResult;          //висота сегменту
+	double sum = 0;               //сума площ сегментів(результат розрахунку)
+	double x = firstArgument + segmentValue;
+
+	while (x <= lastArgument) {
+		functionResult = myFunction(x);
+		sum += functionResult;
+		x += segmentValue;
+	}
+
+	return segmentValue * sum; //площа сегментів
+}
+
+double method_C(double firstArgument, double lastArgument, int precision) {
+	double segmentValue = (lastArgument - firstArgument) / precision;
+	double functionResult1;
+	double functionResult2;
+	double sum = 0;
+	double x = firstArgument;
+
+	while (x <= lastArgument) {
+		functionResult1 = myFunction(x);
+		x += segmentValue;
+		functionResult2 = myFunction(x);
+		sum += (functionResult1 + functionResult2) / 2;
+	}
+
+	return segmentValue * sum;
+}
+
+double method_D(double firstArgument, double lastArgument, int precision) {
+	double segmentValue = (lastArgument - firstArgument) / precision;
+	double x0 = firstArgument;
+	double xn = lastArgument;
+	double x = firstArgument + segmentValue;
+	double sumOdd = 0; //сума непарних складових
+	double sumEven = 0; //сума парних складових
+	int k = 1;
+
+	do {
+		sumOdd += myFunction(x);
+		x += 2 * segmentValue;
+		k += 2;
+	} while (k <= precision - 1);
+
+	k = 2;
+	x = firstArgument + 2 * segmentValue;
+
+	do {
+		sumEven += myFunction(x);
+		x += 2 * segmentValue;
+		k += 2;
+	} while (k <= precision - 2);
+
+	return (segmentValue / 3) * (myFunction(x0) + 4 * sumOdd + 2 * sumEven + myFunction(xn));
+}
+
+
+double calculateByMethod(int methodNumber, float firstArgument, float lastArgument, int precision) {
+	if (methodNumber == 1) {
+		return method_A(firstArgument, lastArgument, precision);
+	}
+	if (methodNumber == 2) {
+		return method_B(firstArgument, lastArgument, precision);
+	}
+	if (methodNumber == 3) {
+		return method_C(firstArgument, lastArgument, precision);
+	}
+	if (methodNumber == 4) {
+		return method_D(firstArgument, lastArgument, precision);
+	}
+}
---
+++ lab_2_2/main.c
@@ -0,0 +1,47 @@
+﻿#include<stdlib.h>  //директива препроцесора
+#include<stdio.h>
+#include<math.h>
+#include"lab_2_calc_integral.h"
+
+
+int main() {
+	float firstArgument = -1.4;
+	float lastArgument = 3.14;
+	int method;
+	int precision;
+	double result;
+	double delta = 100;
+	double L1;
+	double L2;
+
+	//вибір методу інтегрування
+	while (1) {
+		printf("Enter calculation method (1 - 4)\n");
+		scanf_s("%u", &method);
+		if (method < 1 || method > 4) {
+			printf("Method is not supported\n");
+		}
+		else {
+			break;
+		}
+	}
+
+	//налаштування точності інтегрування
+	printf("Enter segments number\n");
+	scanf_s("%u", &precision);
+
+	//перевірка похибки
+	do {
+		L1 = calculateByMethod(method, firstArgument, lastArgument, precision);
+		precision += 2;
+		L2 = calculateByMethod(method, firstArgument, lastArgument, precision);
+		delta = abs(L1 - L2);
+		//printf("delta = %lf\n", delta);
+	} while (delta > 0.001 || delta > 0.00001);
+	precision -= 2;
+	
+	//вивід результату інтегралу
+	printf("result = %lf, precision = %u\n", L2, precision);
+
+	return 0;
+}
